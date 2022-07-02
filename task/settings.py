881c7d56531f5c1049ba4da5c878481fab6086d0
marwan mohamed <38116633+marawan6569@users.noreply.github.com>
2022-07-02 12:58:20 +0200

    Update settings.py

--- task/settings.py
+++ task/settings.py
@@ -66,7 +66,7 @@ ROOT_URLCONF = 'task.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['task/template'],
+        'DIRS': ['template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
